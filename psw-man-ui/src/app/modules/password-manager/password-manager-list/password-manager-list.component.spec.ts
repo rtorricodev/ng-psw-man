661e041cd8ae9a9c20d1223eb7f62a030dc3236a
torricr <torricr@labcorp.com>
2022-09-30 01:25:55 -0400

    scaffold(ui)

---
+++ psw-man-ui/src/app/modules/password-manager/password-manager-list/password-manager-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PasswordManagerListComponent } from './password-manager-list.component';
+
+describe('PasswordManagerListComponent', () => {
+  let component: PasswordManagerListComponent;
+  let fixture: ComponentFixture<PasswordManagerListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PasswordManagerListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PasswordManagerListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ psw-man-ui/src/app/modules/password-manager/password-manager-list/password-manager-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-manager-list',
+  templateUrl: './password-manager-list.component.html',
+  styleUrls: ['./password-manager-list.component.scss']
+})
+export class PasswordManagerListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
