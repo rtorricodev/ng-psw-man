88f420494270a1c5aca21fd0409432b4e70ba2c6
torricr <torricr@labcorp.com>
2022-09-30 15:42:12 -0400

    refactor(confirmation-dialog component)

--- psw-man-ui/src/app/shared/confirmation-dialog/confirmation-dialog.component.ts
+++ psw-man-ui/src/app/shared/confirmation-dialog/confirmation-dialog.component.ts
@@ -4,15 +4,10 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-confirmation-dialog',
   templateUrl: './confirmation-dialog.component.html',
-  styleUrls: ['./confirmation-dialog.component.scss']
+  styleUrls: ['./confirmation-dialog.component.scss'],
 })
 export class ConfirmationDialogComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string }) {}
 
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: { message: string },
-  ) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
