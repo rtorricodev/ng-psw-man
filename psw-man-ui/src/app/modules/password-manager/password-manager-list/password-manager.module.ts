0598f66d3594cfd94cbe54c6bb6b969684a89f14
torricr <torricr@labcorp.com>
2022-09-30 09:11:11 -0400

    added(tailwind)

--- psw-man-ui/src/app/modules/password-manager/password-manager-list/password-manager.module.ts
+++ psw-man-ui/src/app/modules/password-manager/password-manager-list/password-manager.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { PasswordManagerListComponent } from './password-manager/password-manager-list/password-manager-list.component';
+import { PasswordManagerListComponent } from './password-manager-list.component';
 import { PasswordManagerRoutingModule } from './password-manager-router.module';
 
 @NgModule({
