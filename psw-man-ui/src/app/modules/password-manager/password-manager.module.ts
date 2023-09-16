7fc4b3d9f7747c8bb5496828dbacd595e7cdaf37
rtorricodev <rodrigo.torricodelcastillo@gmail.com>
2023-09-16 07:13:47 -0400

    updated angular material 15

--- psw-man-ui/src/app/modules/password-manager/password-manager.module.ts
+++ psw-man-ui/src/app/modules/password-manager/password-manager.module.ts
@@ -3,7 +3,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ClipboardModule } from 'ngx-clipboard';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
 import { NgModule } from '@angular/core';
 import { PasswordManagerCardComponent } from './password-manager-list/password-manager-card/password-manager-card.component';
 import { PasswordManagerFormComponent } from './password-manager-form/password-manager-form.component';
